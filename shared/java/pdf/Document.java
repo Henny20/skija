623737288b97a54bf336bfd0a4292eea50eb9de5
Henny20 <47795195+Henny20@users.noreply.github.com>
2021-09-29 18:26:05 +0200

    Create Document.java

---
+++ shared/java/pdf/Document.java
@@ -0,0 +1,68 @@
+package org.jetbrains.skija.pdf;
+
+import java.lang.ref.*;
+import org.jetbrains.skija.Canvas;
+import org.jetbrains.skija.Rect;
+import org.jetbrains.skija.WStream;
+import org.jetbrains.skija.impl.*;
+
+
+public class Document extends Managed {
+   static { Library.staticLoad(); }
+
+       
+   public Document(long ptr) {
+          super(ptr, _FinalizerHolder.PTR);
+    }
+    
+    public static class _FinalizerHolder {
+        public static final long PTR = _nGetFinalizer();
+    }
+   
+   public Document(WStream stream) {
+        this(_nMake(Native.getPtr(stream)));
+      //  Stats.onNativeCall();
+    }
+    
+    /**   
+    public Kanvas beginPage(float width, float height, float left, float top, float right, float bottom) {
+       Stats.onNativeCall();
+       long cPtr = _nBeginPage(_ptr, width, height, left, top, right, bottom);
+       return (cPtr == 0) ? null : new Kanvas(cPtr, false);
+     }
+    **/
+    public Canvas beginPage(float width, float height) {
+        Stats.onNativeCall();
+       return new Canvas(_nBeginPage(_ptr, this, width, height), true, this);
+       // return new Kanvas(_nBeginPage(_ptr, this, width, height), false);
+    }
+ 
+    public void endPage() {
+        Stats.onNativeCall();
+        _nEndPage(_ptr);
+    }
+
+    public void writeTo(String path) {
+        Stats.onNativeCall();
+        _nWrite(_ptr, path);
+    }
+
+    public void close() {
+        Stats.onNativeCall();
+        _nClose(_ptr);
+    }
+
+    public static native long _nMake(long wstreamPtr);
+
+    public static native long _nGetFinalizer();
+    
+ //  public static native long _nBeginPage(long ptr, float width, float height, float left, float top, float right, float bottom);
+
+    public static native long _nBeginPage(long ptr, Document iets, float width, float height);
+
+    public static native void _nEndPage(long ptr);
+
+    public static native void _nWrite(long ptr, String path);
+
+    public static native void _nClose(long ptr);
+}
